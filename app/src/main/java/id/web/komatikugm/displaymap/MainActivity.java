861d34d8635db52ef5068b14c21f6eee8537adab
Arif Hidayat <arif.h@mail.ugm.ac.id>
2018-12-30 10:11:17 +0700

    first commit

---
+++ app/src/main/java/id/web/komatikugm/displaymap/MainActivity.java
@@ -0,0 +1,37 @@
+package id.web.komatikugm.displaymap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        mMapView.setMap(map);
+    }
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.dispose();
+    }
+}
